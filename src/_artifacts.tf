227b071c610093eb12aa9214e508838612103cb5
amy null <null@amy.gg>
2023-04-24 10:04:28 -0400

    🐛 fix: Fix artifact formats

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -22,3 +22,31 @@ resource "massdriver_artifact" "api_gateway" {
     }
   )
 }
+
+resource "massdriver_artifact" "api" {
+  field                = "api"
+  provider_resource_id = module.api_gateway.arn
+  name                 = "Api Authentication: ${var.md_metadata.name_prefix}"
+  artifact = jsonencode(
+    {
+      data = {
+        api = {
+          hostname = var.dns.enabled ? "${var.dns.sub_domain}.${data.aws_route53_zone.lookup[0].name}" : "${local.api_id}.execute-api.${var.rest_api.region}.amazonaws.com/${var.rest_api.stage_name}"
+          port     = 443
+          protocol = "https"
+        }
+        etc = {
+          api_key = aws_api_gateway_api_key.api_key.value
+        }
+      }
+      specs = {
+        api = {
+          version = "1"
+        }
+        aws = {
+          region = var.rest_api.region
+        }
+      }
+    }
+  )
+}
